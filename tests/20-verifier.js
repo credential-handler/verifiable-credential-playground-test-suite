80821288d768bf60c7ea5ade2c48e537ae7cbc4b
Brian Richter <brianorwhatever@gmail.com>
2023-05-10 09:27:25 -0700

    Add verifiers and verifier tests to test suite.

--- tests/20-verifier.js
+++ tests/20-verifier.js
@@ -1,63 +1,99 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
-import chai from 'chai';
+import {
+  shouldBeValidVerification,
+  shouldBeVerifiedVP,
+  shouldReturnResult,
+  shouldThrowInvalidInput
+} from './assertions.js';
 import configs from '../configs/index.cjs';
+import {createPresentationRequestBody} from './mock.data.js';
 import {Verifier} from '../lib/verifier.js';
-const should = chai.should();
 
 describe('Verify Presentation - Data Integrity', function() {
   for(const [name, config] of Object.entries(configs)) {
     const verifier = new Verifier({config: config.verifier});
     describe(`Verifier: ${name}`, function() {
-      it(`MUST verify a Verifiable Presentation where the credential's issuer,
-          presentation's holder and credential's subject are all different.`,
+      it(`MUST verify a Verifiable Presentation where the presentation's holder
+          and the credential's subject are different.`,
       async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-different');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeVerifiedVP({verification});
       });
-      it(`MUST verify a Verifiable Presentation where the credential's issuer
-          and presentation's holder are the same while the credential's subject
-          is different.`, async function() {
-        should.equal(true, false);
-      });
-      it(`MUST verify a Verifiable Presentation where the credential's issuer
-          and presentation's holder are the same while the credential's subject
-          is different.`, async function() {
-        should.equal(true, false);
-      });
-      it(`MUST verify a Verifiable Presentation where the presentation's holder,
-          credential's subject and issuer are all different.`,
+      it(`MUST verify a Verifiable Presentation where the presentation's holder
+          and the credential's subject are the same.`,
       async function() {
-        should.equal(true, false);
-      });
-      it(`MUST verify a Verifiable Presentation where the presentation's holder,
-          credential's subject and issuer are all the same.`,
-      async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-same');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeVerifiedVP({verification});
       });
       it(`MUST adhere to the proof verification format.`, async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-same');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeValidVerification({verification});
       });
       it(`MUST return a 400 response status code when the request is rejected
           with bad input.`,
       async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-same');
+        delete body.verifiablePresentation;
+        const {result, error} = await verifier.post({
+          json: body
+        });
+        shouldThrowInvalidInput({result, error});
       });
       it(`MUST return a 400 response status code when the request fails
           verification.`, async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-same');
+        body.verifiablePresentation.proof.proofValue = 'badSignatureValue';
+        const {result, error} = await verifier.post({
+          json: body
+        });
+        shouldThrowInvalidInput({result, error});
       });
       it(`MUST support the verification of a Data Integrity proof of type
           "Ed25519Signature2020".`, async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-same');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeVerifiedVP({verification});
       });
       it(`MUST support the verification of a Data Integrity proof of type
           "eddsa-2022".`, async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-eddsa-2022');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeVerifiedVP({verification});
       });
       it(`MUST support the verification of a Data Integrity proof of type
           "ecdsa-2019".`, async function() {
-        should.equal(true, false);
+        const body = createPresentationRequestBody('vp-ecdsa-2019');
+        const {result, data: verification, error} = await verifier.post({
+          json: body
+        });
+        shouldReturnResult({result, error});
+        result.status.should.equal(200, 'Expected statusCode 200.');
+        shouldBeVerifiedVP({verification});
       });
     });
   }
--- tests/assertions.js
+++ tests/assertions.js
@@ -6,8 +6,8 @@ import chai from 'chai';
 const should = chai.should();
 
 export function shouldThrowInvalidInput({result, error}) {
-  should.not.exist(result, 'Expected no result from issuer.');
-  should.exist(error, 'Expected issuer to Error.');
+  should.not.exist(result, 'Expected no result from issuer/verifier.');
+  should.exist(error, 'Expected issuer/verifier to Error.');
   should.exist(error.status, 'Expected an HTTP error response code.');
   error.status.should.not.equal(401,
     'Should not get an Authorization Error.');
@@ -16,26 +16,26 @@ export function shouldThrowInvalidInput({result, error}) {
 }
 
 export function shouldReturnResult({result, error}) {
-  should.not.exist(error, `Expected no error, got ${error?.message}`);
+  if(error) {
+    console.error(JSON.stringify(error.data));
+    should.not.exist(error, `Expected no error, got ${error.message}}`);
+  }
   should.exist(result, 'Expected a result');
 }
 
 export function shouldBeIssuedVc({issuedVc}) {
   issuedVc.should.be.an(
     'object',
-    'Expected the issued Verifiable Credential to be an object.'
-  );
+    'Expected the issued Verifiable Credential to be an object.');
   issuedVc.should.have.property('@context');
   issuedVc.should.have.property('type');
   issuedVc.type.should.contain(
     'VerifiableCredential',
-    'Expected `type` to contain "VerifiableCredential".'
-  );
+    'Expected `type` to contain "VerifiableCredential".');
   issuedVc.should.have.property('id');
   issuedVc.id.should.be.a(
     'string',
-    'Expected `id` to be a string.'
-  );
+    'Expected `id` to be a string.');
   issuedVc.should.have.property('credentialSubject');
   _shouldBeValidCredentialSubject(
     {credentialSubject: issuedVc.credentialSubject});
@@ -43,13 +43,11 @@ export function shouldBeIssuedVc({issuedVc}) {
   const issuerType = typeof(issuedVc.issuer);
   issuerType.should.be.oneOf(
     ['string', 'object'],
-    'Expected `issuer` to be a string or an object.'
-  );
+    'Expected `issuer` to be a string or an object.');
   issuedVc.should.have.property('proof');
   issuedVc.proof.should.be.an(
     'object',
-    'Expected `proof` to be an object.'
-  );
+    'Expected `proof` to be an object.');
   if(issuerType === 'object') {
     should.exist(issuedVc.issuer.id,
       'Expected issuer object to have property id');
@@ -66,8 +64,7 @@ function _shouldBeValidCredentialSubject({credentialSubject}) {
   // a credentialSubject can be an Array of objects
   credentialSubject.length.should.be.gt(
     0,
-    'Expected credentialSubject to make a claim on at least one subject.'
-  );
+    'Expected credentialSubject to make a claim on at least one subject.');
   for(const subject of credentialSubject) {
     _shouldHaveClaims({subject});
   }
@@ -76,10 +73,46 @@ function _shouldBeValidCredentialSubject({credentialSubject}) {
 function _shouldHaveClaims({subject}) {
   subject.should.be.an(
     'object',
-    'Expected credentialSubject to be an object.'
-  );
+    'Expected credentialSubject to be an object.');
   Object.keys(subject).length.should.be.gt(
     0,
-    'Expected credentialSubject to have at least one claim.'
-  );
+    'Expected credentialSubject to have at least one claim.');
+}
+
+export function shouldBeValidVerification({verification}) {
+  verification.should.be.an(
+    'object',
+    'Expected the verification result to be an object.');
+  verification.should.have.property('presentationResult');
+  verification.presentationResult.should.have.property('verified');
+  verification.should.have.property('checks');
+  verification.should.have.property('credentialResults');
+  verification.credentialResults.should.be.a(
+    'array',
+    'Expected `credentialResults` to be an array.');
+  verification.credentialResults.forEach(c => {
+    c.should.be.a(
+      'object',
+      'Expected `credentialResults` items to be objects.');
+    c.should.have.property('verified');
+  });
+}
+
+export function shouldBeVerifiedVP({verification}) {
+  shouldBeValidVerification({verification});
+  verification.should.have.property('verified');
+  verification.verified.should.equal(
+    true,
+    'Expected `verified` to be true.');
+  verification.presentationResult.verified.should.equal(
+    true,
+    'Expected `presentationResult.verified` to be true.');
+  verification.checks.should.contain(
+    'proof',
+    'Expected `checks` to contain "proof".');
+  verification.credentialResults.forEach(c => {
+    c.verified.should.equal(
+      true,
+      'Expected credentialResults `verified` to be true');
+  });
 }
