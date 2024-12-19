712fda51bf04910cab400f115ca41716510bfe82
Benjamin Young <byoung@digitalbazaar.com>
2024-12-19 14:07:46 -0500

    Support `.json` implementation descriptions.
    Can co-exist with `.cjs` descriptions, but `.json` descriptions are the
    final authority.

--- configs/index.cjs
+++ configs/index.cjs
@@ -4,4 +4,9 @@ require('fs').readdirSync(__dirname + '/').forEach(function(file) {
     const name = file.replace('.cjs', '');
     exports[name] = require('./' + file);
   }
+  // JSON-based implementations will override `.cjs` ones
+  if(file.match(/\.json$/) !== null) {
+    const name = file.replace('.json', '');
+    exports[name] = require(`./${file}`);
+  }
 });
